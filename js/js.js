56d7ba977c8a672b8e8c53eb60e20b7ae521b6ab
Alexis Bejarano <82915231+AlexisBejarano@users.noreply.github.com>
2023-03-11 16:16:00 -0700

    Se agrego punto extra
    Todavia sin resolver punto extra

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-let lista = [9,6,5,8,2,1,18,1000,15,0];
+let lista = [9,6,5,8,2,1];
 
 function capturar() {
 
@@ -40,4 +40,30 @@ function ordenarBurbuja() {
         console.log('resultado final '+lista);
     }
     console.log('resultado finallito '+lista);
+}
+
+function ordenarBidireccional() {
+    for (l = 0; l < lista.length - 1; l++) {
+        for (k = 0; k <= lista.length - 2 - l; k++) {
+            if (lista[k] > lista[k + 1]) {
+                let aux = lista[k];
+                lista[k] = lista[k + 1];
+                lista[k + 1] = aux;
+                console.log('resultado final if1 '+lista);
+            }
+            console.log('resultado final for1 '+lista);
+        }
+        for(m=0; m<lista.length-2;m++){
+            if (lista[lista.length-1-m] < lista[lista.length- 2-m]) {
+                let aux2 = lista[lista.length-1-m];
+                lista[lista.length-2-m] = lista[lista.length-1-m];
+                lista[lista.length-1-m] = aux2;
+                console.log('resultado if '+lista);
+            }
+            console.log('resultado del for 2 '+lista);
+        }
+        console.log('resultado final de los dos for '+lista);
+    }
+    console.log('resultado final del for main '+lista);
+    
 }
